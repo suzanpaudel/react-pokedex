04423369a119ab74c428488dc735a745c217adae
Sujan Paudel <sujan2000paudel@gmail.com>
2020-09-06 16:36:45 +0545

    Add Style to Pokemon and Pokedex Page

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,7 +1,128 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import mockData from './mockData';
 
-const Pokedex = () => {
-  return <div>This is a pokedex page</div>;
+const useStyles = makeStyles((theme) => ({
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+  pokedexContainer: {
+    paddingTop: '20px',
+    paddingRight: '40px',
+    paddingLeft: '40px',
+    margin: 0,
+    width: '100%',
+  },
+  center: {
+    margin: 'auto',
+    width: '150px',
+    height: '150px',
+    cursor: 'pointer',
+  },
+  typo: {
+    textTransform: 'capitalize',
+    textAlign: 'center',
+  },
+}));
+
+const Pokedex = (props) => {
+  const { history } = props;
+  const classes = useStyles();
+  const [pokemonData, setPokemonData] = useState(mockData);
+
+  const getPokemonCard = (pokemonId) => {
+    console.log(pokemonData[`${pokemonId}`]);
+    const { id, name } = pokemonData[`${pokemonId}`];
+    const sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+    return (
+      <Grid item xs={6} sm={3} key={pokemonId}>
+        <Card onClick={() => history.push(`/${pokemonId}`)}>
+          <CardMedia image={sprite} className={classes.center}></CardMedia>
+          <CardContent>
+            <Typography
+              className={`${classes.typo}`}
+            >{`${id}. ${name}`}</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  };
+  return (
+    <React.Fragment>
+      <AppBar position='static'>
+        <Toolbar>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder='Search…'
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+      {pokemonData ? (
+        <Grid container spacing={4} className={classes.pokedexContainer}>
+          {Object.keys(pokemonData).map((pokemonId) => {
+            return getPokemonCard(pokemonId);
+          })}
+        </Grid>
+      ) : (
+        <CircularProgress />
+      )}
+    </React.Fragment>
+  );
 };
 
 export default Pokedex;
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,10 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from '@material-ui/core/Avatar';
+import { grey } from '@material-ui/core/colors';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+
+import mockData from './mockData';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 400,
+    margin: '40px auto',
+    boxShadow:
+      '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+  },
+  img: {
+    textAlign: 'center',
+  },
+  cap: {
+    textTransform: 'capitalize',
+  },
+  avatar: {
+    width: '60px',
+    height: '60px',
+    backgroundColor: grey[200],
+  },
+  content: {
+    backgroundColor: '#fafafa',
+    padding: '15px',
+  },
+  typeList: {
+    marginLeft: '20px',
+  },
+}));
 
 const Pokemon = (props) => {
   const { match } = props;
   const { params } = match;
   const { pokeId } = params;
-  return <div>{`This is a pokemon page of id ${pokeId}`}</div>;
+
+  const [pokemon, setPokemon] = useState(mockData[`${pokeId}`]);
+
+  const classes = useStyles();
+  const { name, id, species, height, weight, types, sprites } = pokemon;
+  const fullImgUrl = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
+  const { front_default } = sprites;
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        className={classes.content}
+        avatar={
+          <Avatar className={classes.avatar}>
+            <img src={front_default} alt='pokemon_pic' />
+          </Avatar>
+        }
+        title={
+          <Typography variant='h4' component='h1' className={classes.cap}>
+            {`${id}. ${name}`}
+          </Typography>
+        }
+      />
+      <CardMedia component='div'>
+        <div className={classes.img}>
+          <img src={fullImgUrl} alt={name} height='300px' width='auto' />
+        </div>
+      </CardMedia>
+      <CardContent className={classes.content}>
+        <Typography variant='h4' component='h2'>
+          Pokemon Info
+        </Typography>
+        <Typography>
+          {'Species: '}
+          <Link href={species.url}>{species.name}</Link>
+        </Typography>
+        <Typography>Height: {height}</Typography>
+        <Typography gutterBottom>Weight: {weight}</Typography>
+        <Typography variant='h6'>Types:</Typography>
+        {types.map((typeInfo) => {
+          const { type } = typeInfo;
+          const { name } = type;
+          return (
+            <Typography
+              key={name}
+              className={classes.typeList}
+            >{`- ${name}`}</Typography>
+          );
+        })}
+      </CardContent>
+      <CardActions className={classes.content}>
+        <Button size='small' color='primary' variant='contained'>
+          Back To Pokedex
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default Pokemon;
